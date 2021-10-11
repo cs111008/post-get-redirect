b5fcd356cb752005372c5c2304361b4d33c8374f
“Aman <>
2021-10-11 02:16:02 +0100

    fixing post

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./src/server/server');
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log("App is running on port:", PORT);
